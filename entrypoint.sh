dc96da75e5c4b4196dbdc504a66ec91a9fa69ec2
Pablish <prjr2001@gmail.com>
2025-10-13 13:37:10 -0300

    update archivo de arranque

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-exec php artisan serve --host=0.0.0.0 --port=$PORT
+exec php artisan serve --host=0.0.0.0 --port=${PORT:-9000}
